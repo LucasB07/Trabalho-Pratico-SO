90c2bcb71a4ea9051d9a0f288554ecbdbbe06edf
LucasB07 <lucb0056@gmail.com>
2025-12-09 13:38:25 -0300

    accessPage

---
+++ TrabalhoPraticoSO/TrabalhoPraticoSO/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemandPagingSimulator
+{
+
+    class PageEntry
+    {
+        public int id;
+        public int loadTime;//FIFO
+        public int lastAccessedTime;//LRU
+        public int nextAccessedTime;//OPT
+        
+    }
+
+    class MemoryManager
+    {
+        public int maxFrames;
+        public List<PageEntry> frames = new List<PageEntry>();
+
+        void accessPage(int pageId, int currentTime) 
+        {
+            PageEntry found = null;
+
+            foreach (var page in frames)
+            {
+                if (page.id == pageId)
+                {
+                    found = page;
+                    break;
+                }
+            }
+
+            if (found != null)
+            {
+                found.lastAccessedTime = currentTime;
+                return;
+            }
+
+            if (frames.Count < maxFrames) 
+            {
+                PageEntry newPage = new PageEntry();
+                newPage.id = pageId;
+                newPage.loadTime = currentTime;
+                newPage.lastAccessedTime = currentTime;
+
+                frames.Add(newPage);
+            }
+        }
+
+    }
+}
